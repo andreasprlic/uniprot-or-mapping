d17308e899396df5387334920819b24cdf142fba
Andreas Prlic <andreas.prlic@gmail.com>
2014-10-28 21:51:01 -0700

    fixing bug with gene names

---
+++ src/test/java/TestGeneNames.java
@@ -0,0 +1,25 @@
+import junit.framework.TestCase;
+import org.rcsb.uniprot.auto.dao.UniprotDAO;
+import org.rcsb.uniprot.auto.dao.UniprotDAOImpl;
+
+import java.util.List;
+
+/**
+ * Created by ap3 on 28/10/2014.
+ */
+public class TestGeneNames  extends TestCase {
+
+    public void testHBA1(){
+
+        UniprotDAO dao = new UniprotDAOImpl();
+
+        List<String> geneNames = dao.getGeneNames("P69905");
+
+
+        assertNotNull(geneNames);
+        assertTrue(geneNames.size() >0);
+
+        System.out.println(geneNames);
+
+    }
+}
