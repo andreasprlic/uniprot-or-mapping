7439e51b967671917abc38251a897a4983f6da5e
Andreas Prlic <andreas.prlic@gmail.com>
2014-11-07 15:27:17 -0800

    now much faster access to organism data

---
+++ src/test/java/TestOrganism.java
@@ -0,0 +1,41 @@
+import junit.framework.TestCase;
+import org.rcsb.uniprot.auto.Uniprot;
+import org.rcsb.uniprot.auto.dao.UniprotDAO;
+import org.rcsb.uniprot.auto.dao.UniprotDAOImpl;
+import org.rcsb.uniprot.auto.tools.HibernateUtilsUniprot;
+import org.rcsb.uniprot.auto.tools.JpaUtilsUniProt;
+
+import javax.persistence.EntityManager;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by ap3 on 07/11/2014.
+ */
+public class TestOrganism extends TestCase {
+
+    public void testHBB(){
+
+        String geneName = "HBB";
+        UniprotDAO dao = new UniprotDAOImpl();
+
+        List<String> data = dao.getUniProtACsByGeneName(geneName);
+
+//        System.out.println(data.size());
+//
+//        for ( String ac : data){
+//            System.out.println(ac + " " + dao.getOrganism(ac) + " " + dao.getCommonName(dao.getOrganism(ac)));
+//        }
+
+        String rat = "P02091";
+
+
+        // the rat entry has lower casecharacters in the gene name: Hbb
+        assertTrue(data.contains(rat));
+
+        // this is an old identifier for the rat entry:
+        assertFalse(data.contains("P33584"));
+
+        assertEquals(dao.getOrganism(rat),"Rattus norvegicus");
+    }
+}
