e9848fb47f13007f11114e151ed234d88b0b15e9
Andreas Prlic <andreas.prlic@gmail.com>
2015-09-04 12:05:09 -0700

    fixing another sql

--- src/test/java/TestMixedThings.java
+++ src/test/java/TestMixedThings.java
@@ -72,7 +72,8 @@ public class TestMixedThings  extends TestCase{
 
         UniprotDAO dao = new UniprotDAOImpl();
 
-        dao.getAllGeneNames();
+        //dao.getAllGeneNames();
+        dao.getRecommendedNames4Components();
 //        dao.getAllUniProtIDs();
 //        dao.getAlternativeNameMap();
 //        dao.getECNumbers4Components();
